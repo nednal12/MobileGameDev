159f712c2696c0034c593f916a2e7e5e2efd4531
Brent Marohnic <nednal12@fullsail.edu>
2014-05-15 16:47:41 -0400

    Immersive Element Commit

---
+++ GameConcept/GameConcept/ImmersiveScene.h
@@ -0,0 +1,16 @@
+//
+//  ImmersiveClass.h
+//  GameConcept
+//
+//  Created by Brent Marohnic on 5/12/14.
+//  Copyright 2014 Brent Marohnic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface ImmersiveClass : CCNode {
+    
+}
+
+@end
---
+++ GameConcept/GameConcept/ImmersiveScene.m
@@ -0,0 +1,14 @@
+//
+//  ImmersiveClass.m
+//  GameConcept
+//
+//  Created by Brent Marohnic on 5/12/14.
+//  Copyright 2014 Brent Marohnic. All rights reserved.
+//
+
+#import "ImmersiveClass.h"
+
+
+@implementation ImmersiveClass
+
+@end
